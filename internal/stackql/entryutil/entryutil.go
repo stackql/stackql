stackql/stackql-devel

pg-pass-through
Summary:

  - When using `postgres` as SQL backend, then pass through identifiable housekeeping queries.
  - Housekeeping queries can be identified prior to AST visitor passes.
    - Queries thus identified can pre-empt subsequent analysis and proceed straight to execution against `postgres`.
  - **TECH DEBT**: parser does not assemble viable trees for all housekeeping queries.
    - Existing `postgres` formatter throws panic from this.
    - RCA and fix should be done later but priority lower due to above pre-emption of analysis.

internal/stackql/entryutil/entryutil.go
@@ -15,6 +15,7 @@ import (
 	"github.com/stackql/stackql/internal/stackql/handler"
 	"github.com/stackql/stackql/internal/stackql/iqlerror"
 	"github.com/stackql/stackql/internal/stackql/kstore"
+	"github.com/stackql/stackql/internal/stackql/pginternal"
 	"github.com/stackql/stackql/internal/stackql/sqlcontrol"
 	"github.com/stackql/stackql/internal/stackql/sqldialect"
 	"github.com/stackql/stackql/internal/stackql/sqlengine"
@@ -52,6 +53,11 @@ func BuildInputBundle(runtimeCtx dto.RuntimeCtx) (bundle.Bundle, error) {
 	if err != nil {
 		return nil, err
 	}
+	pgInternalCfg, err := dto.GetPGInternalCfg(runtimeCtx.DBInternalCfgRaw)
+	if err != nil {
+		return nil, err
+	}
+	pgInternal, err := pginternal.GetPGInternalRouter(pgInternalCfg, dialect)
 	namespaces, err = namespaces.WithSQLDialect(dialect)
 	if err != nil {
 		return nil, err
@@ -69,7 +75,7 @@ func BuildInputBundle(runtimeCtx dto.RuntimeCtx) (bundle.Bundle, error) {
 	if err != nil {
 		return nil, err
 	}
-	return bundle.NewBundle(gc, namespaces, se, dialect, controlAttributes, txnStore, txnCtrMgr), nil
+	return bundle.NewBundle(gc, namespaces, se, dialect, pgInternal, controlAttributes, txnStore, txnCtrMgr), nil
 }
 
 func initNamespaces(namespaceCfgRaw string, sqlEngine sqlengine.SQLEngine) (tablenamespace.TableNamespaceCollection, error) {

internal/stackql/planbuilder/plan_builder.go
@@ -289,6 +289,20 @@ func (pgb *planGraphBuilder) nop(pbi PlanBuilderInput) error {
 	return err
 }
 
+func (pgb *planGraphBuilder) pgInternal(pbi PlanBuilderInput) error {
+	primitiveGenerator := newRootPrimitiveGenerator(nil, pbi.GetHandlerCtx(), pgb.planGraph)
+	err := primitiveGenerator.analyzePGInternal(pbi)
+	if err != nil {
+		return err
+	}
+	builder := primitiveGenerator.PrimitiveComposer.GetBuilder()
+	if builder == nil {
+		return fmt.Errorf("nil pg internal builder")
+	}
+	err = builder.Build()
+	return err
+}
+
 func (pgb *planGraphBuilder) handleAuth(pbi PlanBuilderInput) error {
 	handlerCtx := pbi.GetHandlerCtx()
 	node, ok := pbi.GetAuth()
@@ -400,7 +414,9 @@ func (pgb *planGraphBuilder) handleSelect(pbi PlanBuilderInput) (*primitivegraph
 		}
 		builder := primitiveGenerator.PrimitiveComposer.GetBuilder()
 		_, isNativeSelect := builder.(*primitivebuilder.NativeSelect)
-		isLocallyExecutable := !isNativeSelect
+		_, isRawNativeSelect := builder.(*primitivebuilder.RawNativeSelect)
+		_, isRawNativeExec := builder.(*primitivebuilder.RawNativeExec)
+		isLocallyExecutable := !isNativeSelect && !isRawNativeSelect && !isRawNativeExec
 		// check tables only if not native
 		if isLocallyExecutable {
 			for _, val := range primitiveGenerator.PrimitiveComposer.GetTables() {
@@ -907,6 +923,33 @@ func BuildPlanFromContext(handlerCtx *handler.HandlerContext) (*plan.Plan, error
 	qPlan.Type = statementType
 
 	pGBuilder := newPlanGraphBuilder(handlerCtx.RuntimeContext.ExecutionConcurrencyLimit)
+
+	// Before analysing AST, see if we can pass stright to SQL backend
+	opType, ok := handlerCtx.GetPGInternalRouter().CanRoute(result.AST)
+	if ok {
+		logging.GetLogger().Debugf("%v", opType)
+		pbi, err := NewPlanBuilderInput(handlerCtx, result.AST, nil, nil, nil, nil, nil, *tcc)
+		if err != nil {
+			return nil, err
+		}
+		createInstructionError := pGBuilder.pgInternal(pbi)
+		if createInstructionError != nil {
+			return nil, createInstructionError
+		}
+		qPlan.Instructions = pGBuilder.planGraph
+
+		if qPlan.Instructions != nil {
+			err = qPlan.Instructions.Optimise()
+			if err != nil {
+				return createErroneousPlan(handlerCtx, qPlan, rowSort, err)
+			}
+			if qPlan.IsCacheable() {
+				handlerCtx.LRUCache.Set(planKey, qPlan)
+			}
+		}
+		return qPlan, err
+
+	}
 
 	// First pass AST analysis; extract provider strings for auth.
 	provStrSlice, cacheExemptMaterialDetected := astvisit.ExtractProviderStringsAndDetectCacheExceptMaterial(result.AST, handlerCtx.SQLDialect, handlerCtx.GetASTFormatter(), handlerCtx.GetNamespaceCollection())

internal/stackql/planbuilder/statement_analyzer.go
@@ -775,12 +775,15 @@ func (p *primitiveGenerator) analyzeSchemaVsMap(handlerCtx *handler.HandlerConte
 		for k, _ := range requiredElements {
 			missingKeys = append(missingKeys, k)
 		}
-		return fmt.Errorf("required elements not included in suplied object; the following keys are missing: %s.", strings.Join(missingKeys, ", "))
+		return fmt.Errorf("required elements not included in suplied object; the following keys are missing: %s", strings.Join(missingKeys, ", "))
 	}
 	return nil
 }
 
 func isPGSetupQuery(pbi PlanBuilderInput) (nativedb.Select, bool) {
+	handlerCtx := pbi.GetHandlerCtx()
+	routeType, canRoute := handlerCtx.GetPGInternalRouter().CanRoute(pbi.GetStatement())
+	logging.GetLogger().Debugf("canRoute = %t, routeType = %v\n", canRoute, routeType)
 	qStripped := multipleWhitespaceRegexp.ReplaceAllString(pbi.GetRawQuery(), " ")
 	if qStripped == "select relname, nspname, relkind from pg_catalog.pg_class c, pg_catalog.pg_namespace n where relkind in ('r', 'v', 'm', 'f') and nspname not in ('pg_catalog', 'information_schema', 'pg_toast', 'pg_temp_1') and n.oid = relnamespace order by nspname, relname" {
 		return nil, true
@@ -818,6 +821,26 @@ func isPGSetupQuery(pbi PlanBuilderInput) (nativedb.Select, bool) {
 	return nil, false
 }
 
+func (p *primitiveGenerator) analyzePGInternal(pbi PlanBuilderInput) error {
+	handlerCtx := pbi.GetHandlerCtx()
+	if backendQueryType, ok := handlerCtx.GetPGInternalRouter().CanRoute(pbi.GetStatement()); ok {
+		if backendQueryType == constants.BackendQuery {
+			bldr := primitivebuilder.NewRawNativeSelect(p.PrimitiveComposer.GetGraph(), handlerCtx, pbi.GetTxnCtrlCtrs(), pbi.GetRawQuery())
+			p.PrimitiveComposer.SetBuilder(bldr)
+			return nil
+		}
+		if backendQueryType == constants.BackendExec {
+			bldr := primitivebuilder.NewRawNativeExec(p.PrimitiveComposer.GetGraph(), handlerCtx, pbi.GetTxnCtrlCtrs(), pbi.GetRawQuery())
+			p.PrimitiveComposer.SetBuilder(bldr)
+			return nil
+		}
+		if backendQueryType == constants.BackendNop {
+			return p.analyzeNop(pbi)
+		}
+	}
+	return fmt.Errorf("cannot execute PG internal")
+}
+
 func (p *primitiveGenerator) analyzeSelect(pbi PlanBuilderInput) error {
 
 	handlerCtx := pbi.GetHandlerCtx()
@@ -826,6 +849,8 @@ func (p *primitiveGenerator) analyzeSelect(pbi PlanBuilderInput) error {
 		return fmt.Errorf("could not cast statement of type '%T' to required Select", pbi.GetStatement())
 	}
 
+	// TODO: get rid of this and dependent tests.
+	// We need not emulate postgres for other backends at this stage.
 	if sel, ok := isPGSetupQuery(pbi); ok {
 		if sel != nil {
 			bldr := primitivebuilder.NewNativeSelect(p.PrimitiveComposer.GetGraph(), handlerCtx, sel)

stackql/stackql

  - Previously failing `sqlalchemy` setup added as a robot test.

internal/stackql/entryutil/entryutil.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/stackql/stackql/internal/stackql/bundle"
+	"github.com/stackql/stackql/internal/stackql/dbmsinternal"
 	"github.com/stackql/stackql/internal/stackql/dto"
 	"github.com/stackql/stackql/internal/stackql/garbagecollector"
 	"github.com/stackql/stackql/internal/stackql/gcexec"
@@ -52,6 +53,11 @@ func BuildInputBundle(runtimeCtx dto.RuntimeCtx) (bundle.Bundle, error) {
 	if err != nil {
 		return nil, err
 	}
+	pgInternalCfg, err := dto.GetDBMSInternalCfg(runtimeCtx.DBInternalCfgRaw)
+	if err != nil {
+		return nil, err
+	}
+	pgInternal, err := dbmsinternal.GetDBMSInternalRouter(pgInternalCfg, dialect)
 	namespaces, err = namespaces.WithSQLDialect(dialect)
 	if err != nil {
 		return nil, err
@@ -69,7 +75,7 @@ func BuildInputBundle(runtimeCtx dto.RuntimeCtx) (bundle.Bundle, error) {
 	if err != nil {
 		return nil, err
 	}
-	return bundle.NewBundle(gc, namespaces, se, dialect, controlAttributes, txnStore, txnCtrMgr), nil
+	return bundle.NewBundle(gc, namespaces, se, dialect, pgInternal, controlAttributes, txnStore, txnCtrMgr), nil
 }
 
 func initNamespaces(namespaceCfgRaw string, sqlEngine sqlengine.SQLEngine) (tablenamespace.TableNamespaceCollection, error) {

internal/stackql/planbuilder/plan_builder.go
@@ -289,6 +289,20 @@ func (pgb *planGraphBuilder) nop(pbi PlanBuilderInput) error {
 	return err
 }
 
+func (pgb *planGraphBuilder) pgInternal(pbi PlanBuilderInput) error {
+	primitiveGenerator := newRootPrimitiveGenerator(nil, pbi.GetHandlerCtx(), pgb.planGraph)
+	err := primitiveGenerator.analyzePGInternal(pbi)
+	if err != nil {
+		return err
+	}
+	builder := primitiveGenerator.PrimitiveComposer.GetBuilder()
+	if builder == nil {
+		return fmt.Errorf("nil pg internal builder")
+	}
+	err = builder.Build()
+	return err
+}
+
 func (pgb *planGraphBuilder) handleAuth(pbi PlanBuilderInput) error {
 	handlerCtx := pbi.GetHandlerCtx()
 	node, ok := pbi.GetAuth()
@@ -400,7 +414,9 @@ func (pgb *planGraphBuilder) handleSelect(pbi PlanBuilderInput) (*primitivegraph
 		}
 		builder := primitiveGenerator.PrimitiveComposer.GetBuilder()
 		_, isNativeSelect := builder.(*primitivebuilder.NativeSelect)
-		isLocallyExecutable := !isNativeSelect
+		_, isRawNativeSelect := builder.(*primitivebuilder.RawNativeSelect)
+		_, isRawNativeExec := builder.(*primitivebuilder.RawNativeExec)
+		isLocallyExecutable := !isNativeSelect && !isRawNativeSelect && !isRawNativeExec
 		// check tables only if not native
 		if isLocallyExecutable {
 			for _, val := range primitiveGenerator.PrimitiveComposer.GetTables() {
@@ -907,6 +923,33 @@ func BuildPlanFromContext(handlerCtx *handler.HandlerContext) (*plan.Plan, error
 	qPlan.Type = statementType
 
 	pGBuilder := newPlanGraphBuilder(handlerCtx.RuntimeContext.ExecutionConcurrencyLimit)
+
+	// Before analysing AST, see if we can pass stright to SQL backend
+	opType, ok := handlerCtx.GetDBMSInternalRouter().CanRoute(result.AST)
+	if ok {
+		logging.GetLogger().Debugf("%v", opType)
+		pbi, err := NewPlanBuilderInput(handlerCtx, result.AST, nil, nil, nil, nil, nil, *tcc)
+		if err != nil {
+			return nil, err
+		}
+		createInstructionError := pGBuilder.pgInternal(pbi)
+		if createInstructionError != nil {
+			return nil, createInstructionError
+		}
+		qPlan.Instructions = pGBuilder.planGraph
+
+		if qPlan.Instructions != nil {
+			err = qPlan.Instructions.Optimise()
+			if err != nil {
+				return createErroneousPlan(handlerCtx, qPlan, rowSort, err)
+			}
+			if qPlan.IsCacheable() {
+				handlerCtx.LRUCache.Set(planKey, qPlan)
+			}
+		}
+		return qPlan, err
+
+	}
 
 	// First pass AST analysis; extract provider strings for auth.
 	provStrSlice, cacheExemptMaterialDetected := astvisit.ExtractProviderStringsAndDetectCacheExceptMaterial(result.AST, handlerCtx.SQLDialect, handlerCtx.GetASTFormatter(), handlerCtx.GetNamespaceCollection())

internal/stackql/planbuilder/statement_analyzer.go
@@ -775,12 +775,15 @@ func (p *primitiveGenerator) analyzeSchemaVsMap(handlerCtx *handler.HandlerConte
 		for k, _ := range requiredElements {
 			missingKeys = append(missingKeys, k)
 		}
-		return fmt.Errorf("required elements not included in suplied object; the following keys are missing: %s.", strings.Join(missingKeys, ", "))
+		return fmt.Errorf("required elements not included in suplied object; the following keys are missing: %s", strings.Join(missingKeys, ", "))
 	}
 	return nil
 }
 
 func isPGSetupQuery(pbi PlanBuilderInput) (nativedb.Select, bool) {
+	handlerCtx := pbi.GetHandlerCtx()
+	routeType, canRoute := handlerCtx.GetDBMSInternalRouter().CanRoute(pbi.GetStatement())
+	logging.GetLogger().Debugf("canRoute = %t, routeType = %v\n", canRoute, routeType)
 	qStripped := multipleWhitespaceRegexp.ReplaceAllString(pbi.GetRawQuery(), " ")
 	if qStripped == "select relname, nspname, relkind from pg_catalog.pg_class c, pg_catalog.pg_namespace n where relkind in ('r', 'v', 'm', 'f') and nspname not in ('pg_catalog', 'information_schema', 'pg_toast', 'pg_temp_1') and n.oid = relnamespace order by nspname, relname" {
 		return nil, true
@@ -818,6 +821,26 @@ func isPGSetupQuery(pbi PlanBuilderInput) (nativedb.Select, bool) {
 	return nil, false
 }
 
+func (p *primitiveGenerator) analyzePGInternal(pbi PlanBuilderInput) error {
+	handlerCtx := pbi.GetHandlerCtx()
+	if backendQueryType, ok := handlerCtx.GetDBMSInternalRouter().CanRoute(pbi.GetStatement()); ok {
+		if backendQueryType == constants.BackendQuery {
+			bldr := primitivebuilder.NewRawNativeSelect(p.PrimitiveComposer.GetGraph(), handlerCtx, pbi.GetTxnCtrlCtrs(), pbi.GetRawQuery())
+			p.PrimitiveComposer.SetBuilder(bldr)
+			return nil
+		}
+		if backendQueryType == constants.BackendExec {
+			bldr := primitivebuilder.NewRawNativeExec(p.PrimitiveComposer.GetGraph(), handlerCtx, pbi.GetTxnCtrlCtrs(), pbi.GetRawQuery())
+			p.PrimitiveComposer.SetBuilder(bldr)
+			return nil
+		}
+		if backendQueryType == constants.BackendNop {
+			return p.analyzeNop(pbi)
+		}
+	}
+	return fmt.Errorf("cannot execute PG internal")
+}
+
 func (p *primitiveGenerator) analyzeSelect(pbi PlanBuilderInput) error {
 
 	handlerCtx := pbi.GetHandlerCtx()
@@ -826,6 +849,8 @@ func (p *primitiveGenerator) analyzeSelect(pbi PlanBuilderInput) error {
 		return fmt.Errorf("could not cast statement of type '%T' to required Select", pbi.GetStatement())
 	}
 
+	// TODO: get rid of this and dependent tests.
+	// We need not emulate postgres for other backends at this stage.
 	if sel, ok := isPGSetupQuery(pbi); ok {
 		if sel != nil {
 			bldr := primitivebuilder.NewNativeSelect(p.PrimitiveComposer.GetGraph(), handlerCtx, sel)