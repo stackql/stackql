bce633040de25b66b860a8be41a243d04bd282df
sanketmp <sanketmpol@gmail.com>
2023-10-16 11:24:28 +0530

    add unit test for templatenamespace update 2(after golangci-lint run)

--- internal/stackql/templatenamespace/template_namespace_configurator_test.go
+++ internal/stackql/templatenamespace/template_namespace_configurator_test.go
@@ -4,85 +4,84 @@ import (
 	"regexp"
 	"testing"
 	"text/template"
-
 )
 
-func TestGetObjectName(t *testing.T){
+func TestGetObjectName(t *testing.T) {
 	regex := regexp.MustCompile(`(?P<objectName>\w+)`)
 	templ := template.Must(template.New("test").Parse("{{.objectName}}"))
-	config, err := NewTemplateNamespaceConfigurator(regex,templ)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cases:= []struct {
-		input string
-		expexted string
-	}{
-		{"one","one"},
-		{"two","two"},
-		{"abc123","abc123"},
-		{"",""},
-	}
-
-	for _,c:= range cases{
-		actual := config.GetObjectName(c.input)
-		if actual != c.expexted{
-			t.Errorf("GetObjectNAme(%q) = %q, wants %q",c.input,actual,c.expexted)
-		}
-	}
-}
-
-func TestRenderTemplate(t *testing.T){
-	regex := regexp.MustCompile(`(?P<objectName>\w+)`)
-	templete := template.Must(template.New("test").Parse("TestRenderTemplate's {{.objectName}}"))
-	config,err := NewTemplateNamespaceConfigurator(regex,templete)
+	config, err := NewTemplateNamespaceConfigurator(regex, templ)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	cases := []struct {
-		input string
-		expected string
+		input    string
+		expexted string
 	}{
-		{"one","TestRenderTemplate's one"},
-		{"two","TestRenderTemplate's two"},
-		{"abc12","TestRenderTemplate's abc12"},
+		{"one", "one"},
+		{"two", "two"},
+		{"abc123", "abc123"},
+		{"", ""},
 	}
 
-	for _,c := range cases{
-		actual,err := config.RenderTemplate(c.input)
+	for _, c := range cases {
+		actual := config.GetObjectName(c.input)
+		if actual != c.expexted {
+			t.Errorf("GetObjectNAme(%q) = %q, wants %q", c.input, actual, c.expexted)
+		}
+	}
+}
+
+func TestRenderTemplate(t *testing.T) {
+	regex := regexp.MustCompile(`(?P<objectName>\w+)`)
+	templete := template.Must(template.New("test").Parse("TestRenderTemplate's {{.objectName}}"))
+	config, err := NewTemplateNamespaceConfigurator(regex, templete)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		input    string
+		expected string
+	}{
+		{"one", "TestRenderTemplate's one"},
+		{"two", "TestRenderTemplate's two"},
+		{"abc12", "TestRenderTemplate's abc12"},
+	}
+
+	for _, c := range cases {
+		actual, err := config.RenderTemplate(c.input)
 		if err != nil {
 			t.Error(err)
 			continue
 		}
-		if actual != c.expected{
-			t.Errorf("RenderTemplate(%q)= %q, and want %q.",c.input,actual,c.expected)
+		if actual != c.expected {
+			t.Errorf("RenderTemplate(%q)= %q, and want %q.", c.input, actual, c.expected)
 		}
 	}
 }
 
-func TestIsAllowed(t *testing.T){
+func TestIsAllowed(t *testing.T) {
 	regex := regexp.MustCompile(`(?P<objectName>\w+)`)
 	temp := template.Must(template.New("test").Parse("{{.objectName}}"))
-	conf,e := NewTemplateNamespaceConfigurator(regex,temp)
-	if e !=nil{
+	conf, e := NewTemplateNamespaceConfigurator(regex, temp)
+	if e != nil {
 		t.Fatal(e)
 	}
 	cases := []struct {
-		input string
+		input    string
 		expected bool
 	}{
-		{"one",true},
-		{"twoo",true},
-		{"abc123",true},
-		{"",false},
+		{"one", true},
+		{"twoo", true},
+		{"abc123", true},
+		{"", false},
 	}
 
-	for _, c:= range cases{
+	for _, c := range cases {
 		actual := conf.IsAllowed(c.input)
-		if actual!= c.expected {
-			t.Errorf("conf.IsAllowed(%q)= %v requires %v",c.input,actual,c.expected)
+		if actual != c.expected {
+			t.Errorf("conf.IsAllowed(%q)= %v requires %v", c.input, actual, c.expected)
 		}
 	}
 }
