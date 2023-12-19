6301594c369703cc44e33c4f9d8a97c07dfbcab0
General Kroll <generalkroll0@gmail.com>
2023-12-19 12:09:45 +1100

    in-clause-defects
    Summary:
    
    - Fix defect that caused nil result sets inside `IN` subclause responses to fail most of the time.
    - Remove spurious control parameter serialization.
    - Expand control parameter equivalency to cover selections.
    - Added robot test `Select Subquery Join With Path Parameters inside IN Scalars Including Empty inside WHERE Clause Returns Expected Result`.

--- internal/stackql/responsehandler/responsehandler.go
+++ internal/stackql/responsehandler/responsehandler.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/stackql/stackql/internal/stackql/handler"
 	"github.com/stackql/stackql/internal/stackql/internal_data_transfer/internaldto"
-	"github.com/stackql/stackql/internal/stackql/logging"
 	"github.com/stackql/stackql/internal/stackql/output"
 )
 
@@ -24,42 +23,42 @@ func handleEmptyWriter(outputWriter output.IOutputWriter, err error) {
 func HandleResponse(handlerCtx handler.HandlerContext, response internaldto.ExecutorOutput) error {
 	var outputWriter output.IOutputWriter
 	var err error
-	logging.GetLogger().Debugln(fmt.Sprintf("response from query = '%v'", response.GetSQLResult()))
 	if response.GetMessages() != nil {
 		for _, msg := range response.GetMessages() {
 			handlerCtx.GetOutErrFile().Write([]byte(msg + fmt.Sprintln(""))) //nolint:errcheck // outstream write
 		}
 	}
-	//nolint:staticcheck // TODO: review
-	if response.GetSQLResult() != nil && response.GetSQLResult() != nil && response.GetError() == nil {
+	sqlResult := response.GetSQLResult()
+	sqlErr := response.GetError()
+	if sqlResult != nil && sqlErr == nil {
 		outputWriter, err = output.GetOutputWriter(
 			handlerCtx.GetOutfile(),
 			handlerCtx.GetOutErrFile(),
 			internaldto.OutputContext{
 				RuntimeContext: handlerCtx.GetRuntimeContext(),
-				Result:         response.GetSQLResult(),
+				Result:         sqlResult,
 			},
 		)
 		if outputWriter == nil || err != nil {
 			handleEmptyWriter(outputWriter, err)
 			return err
 		}
-		outputWriter.Write(response.GetSQLResult()) //nolint:errcheck // outstream write
-	} else if response.GetError() != nil {
+		outputWriter.Write(sqlResult) //nolint:errcheck // outstream write
+	} else if sqlErr != nil {
 		outputWriter, err = output.GetOutputWriter(
 			handlerCtx.GetOutfile(),
 			handlerCtx.GetOutErrFile(),
 			internaldto.OutputContext{
 				RuntimeContext: handlerCtx.GetRuntimeContext(),
-				Result:         response.GetSQLResult(),
+				Result:         sqlResult,
 			},
 		)
 		if outputWriter == nil || err != nil {
 			handleEmptyWriter(outputWriter, err)
-			return response.GetError()
+			return sqlErr
 		}
-		outputWriter.WriteError(response.GetError(), handlerCtx.GetErrorPresentation()) //nolint:errcheck // outstream write
-		return response.GetError()
+		outputWriter.WriteError(sqlErr, handlerCtx.GetErrorPresentation()) //nolint:errcheck // outstream write
+		return sqlErr
 	}
 	return err
 }
