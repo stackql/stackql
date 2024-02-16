afbbc039c3b62a391220610f1a9c8640fb678dfa
Jeffrey Aven <javen@avensolutions.com>
2024-02-17 09:22:32 +1100

    fixed linting errors

--- internal/stackql/primitivebuilder/graphql_single_select_acquire.go
+++ internal/stackql/primitivebuilder/graphql_single_select_acquire.go
@@ -128,7 +128,8 @@ func (ss *GraphQLSingleSelectAcquire) Build() error {
 				ss.handlerCtx.GetGarbageCollector().Update(tableName, olderTcc, currentTcc)
 				ss.insertionContainer.SetTableTxnCounters(tableName, olderTcc)
 				ss.insertPreparedStatementCtx.SetGCCtrlCtrs(olderTcc)
-				r, sqlErr := ss.handlerCtx.GetNamespaceCollection().GetAnalyticsCacheTableNamespaceConfigurator().Read( //nolint:rowserrcheck // TODO: fix this
+				r, sqlErr := ss.handlerCtx.GetNamespaceCollection().GetAnalyticsCacheTableNamespaceConfigurator().Read(
+					//nolint:rowserrcheck // TODO: fix this
 					tableName, reqEncoding,
 					ss.drmCfg.GetControlAttributes().GetControlInsertEncodedIDColumnName(),
 					nonControlColumnNames)
